477e90609a5331c773efd93643cad9e11ec4a8a1
Vishal Canumalla <vishalc@cs.washington.edu>
2024-07-11 14:32:43 -0700

    initial app commit

---
+++ apps/sca/halide_sca_generator.cpp
@@ -0,0 +1,19 @@
+#include "Halide.h"
+namespace {
+class Passthru : public Halide::Generator<Passthru> {
+public:
+    Input<Buffer<int32_t, 2>> input{"input"};
+    Output<Buffer<int32_t, 2>> output{"output"};
+    void generate() {
+        Var x, y;
+        output(x, y) = input(x, y);
+        if (!using_autoscheduler()) {
+            output.parallel(y).vectorize(x, natural_vector_size<int32_t>()).compute_root();
+        } else {
+            input.set_estimates({{0, 256}});
+            output.set_estimates({{0, 256}});
+        }
+    }
+};
+}  // namespace
+HALIDE_REGISTER_GENERATOR(Passthru, passthru) 
