313967bf9ccde8a1d5359f473fabf5ff4df49743
Vishal Canumalla <vishalc@cs.washington.edu>
2024-07-17 16:44:27 -0700

    make it slightly more complex

--- apps/sca/halide_sca_generator.cpp
+++ apps/sca/halide_sca_generator.cpp
@@ -6,13 +6,7 @@ public:
     Output<Buffer<int32_t, 2>> output{"output"};
     void generate() {
         Var x, y;
-        output(x, y) = input(x, y);
-        // if (!using_autoscheduler()) {
-        //     output.parallel(y).vectorize(x, natural_vector_size<int32_t>()).compute_root();
-        // } else {
-        //     input.set_estimates({{0, 256}});
-        //     output.set_estimates({{0, 256}});
-        // }
+        output(x, y) = input(x, y) + 5;
     }
 };
 }  // namespace
