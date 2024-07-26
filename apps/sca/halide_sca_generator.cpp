b3ee2646d8422223cd87a08671df53cb0c02ba87
Vishal Canumalla <vishalc@cs.washington.edu>
2024-07-26 10:33:31 -0700

    local changes for debugging

--- apps/sca/halide_sca_generator.cpp
+++ apps/sca/halide_sca_generator.cpp
@@ -1,12 +1,52 @@
+// #include "Halide.h"
+// namespace {
+// class HalideSCA : public Halide::Generator<HalideSCA> {
+// public:
+//     Input<Buffer<int32_t, 2>> input{"input"};
+//     Output<Buffer<int32_t, 2>> output{"output"};
+//     void generate() {
+//         Var x, y;
+//         output(x, y) = input(x, y) + 5;
+//     }
+// };
+
+
+
+
+
+
+// }  // namespace
+// HALIDE_REGISTER_GENERATOR(HalideSCA, halide_sca) 
+
+
+
+
+
 #include "Halide.h"
 namespace {
 class HalideSCA : public Halide::Generator<HalideSCA> {
 public:
-    Input<Buffer<int32_t, 2>> input{"input"};
-    Output<Buffer<int32_t, 2>> output{"output"};
+    // GeneratorParam<int> tile_x{"tile_x", 32};  // X tile.
+    // GeneratorParam<int> tile_y{"tile_y", 8};   // Y tile.
+
+    Input<Buffer<uint16_t, 2>> input{"input"};
+    Output<Buffer<uint16_t, 2>> blur_y{"blur_y"};
     void generate() {
-        Var x, y;
-        output(x, y) = input(x, y) + 5;
+        Var x("x_dim"), y("y_dim"), yi("yi_inner");
+        Func blur_x("blur_x");
+        // gradient(x, y) = x + y;
+        // gradient.split(x, x_outer, x_inner, 2);
+        // output(x, y) = cast(output.type(), gradient(x, y));
+        blur_x(x, y) = (input(x, y) + input(x + 1, y) + input(x + 2, y)) / 3;
+        blur_y(x, y) = (blur_x(x, y) + blur_x(x, y + 1) + blur_x(x, y + 2)) / 3;
+        blur_y
+            .split(y, y, yi, 32)
+            .parallel(y)
+            .vectorize(x, 16);
+        // blur_x
+        //     .store_at(blur_y, y)
+        //     .compute_at(blur_y, x)
+        //     .vectorize(x, 16);
     }
 };
 }  // namespace
--- src/SymbolicComplexity.cpp
+++ src/SymbolicComplexity.cpp
@@ -13,15 +13,60 @@ namespace Internal {
 
 namespace {
 
-class SymbolicComplexity : public IRVisitor {
+class VariableBindings: public IRVisitor {
     using IRVisitor::visit;
 
+    public:
+        std::map<std::string name, Variable var> bindings;
+}
+class SymbolicComplexity : public IRVisitor {
+    using IRVisitor::visit;
+    Expr inline_expr(Expr e) {
+        // recursive traverse through expr, replacing variables with their bindings
+        debug(1) << "Bindings: \n\n\n\n\n\n\n";
+        for (auto &b : bindings) {
+            debug(1) << "Binding: " << b.first << " | " << b.second << "\n";
+        }
+        debug(1) << "End bindings\n\n\n\n\n\n\n";
+        debug(1) << "Inlining expr: " << e << "\n";
+        Expr new_expr = e;
+        if (e.as<Sub>() != nullptr) {
+            // recursive on left
+            Expr left = e.as<Sub>()->a;
+            Expr right = e.as<Sub>()->b;
+            debug(1) << "left: " << left << " | right: " << right << "\n";
+            left = inline_expr(left);
+            right = inline_expr(right);
+            new_expr = (left - right);
+        }
+        IRNodeType type = e.node_type();
+        // debug(1) << "Type: " << type << "\n";
+        
+        // case for each enum
+        if (type == IRNodeType::IntImm) {
+            debug(1) << "IntImm: " << e << "\n";
+            return e;
+        }
+        if (type == IRNodeType::UIntImm) {
+            debug(1) << "UIntImm: " << e << "\n";
+            return e;
+        }
+        if (type == IRNodeType::FloatImm) {
+            debug(1) << "FloatImm: " << e << "\n";
+            return e;
+        }
+        if (type == IRNodeType::StringImm) {
+            debug(1) << "StringImm: " << e << "\n";
+            return e;
+        }
+        return e;
+    }
     void visit(const For *op) override {
-        debug(2) << "Visiting for loop: " << op->name << " thats initialized " << factor << " times" << "\n";
-        debug(2) << "For loop: " << op->name << " min: " << op->min << " extent: " << op->extent << "\n";
+        debug(1) << "Visiting for loop: " << op->name << " thats initialized " << factor << " times" << "\n";
+        debug(1) << "For loop: " << op->name << " min: " << op->min << " extent: " << op->extent << "\n";
 
         rawFor++;
-        totalRange = op->extent - op->min;
+        totalRange = inline_expr(op->extent - op->min);
         factor = factor * totalRange;
         // run through body, with factor (number of iterations) incremented
         debug(1) << "Running body with factor: " << factor << "\n";
@@ -31,14 +76,15 @@ class SymbolicComplexity : public IRVisitor {
     }
     void visit(const Let *op) override {
         debug(2) << "Visiting let: " << op->name << " thats initialized " << factor << " times" << "\n";
-        debug(2) << "Let: " << op->name << " value: " << op->value << "\n";
+        debug(1) << "Let: " << op->name << " value: " << op->value << "\n";
         op->value.accept(this);
         op->body.accept(this);
     
     }
 
     void visit(const LetStmt *op) override {
-        debug(2) << "Visiting let stmt: " << op->name << ", factor: " << factor << "\n";
+        debug(1) << "Visiting let stmt: " << op->name << ", body: " << op->value << "\n";
+        bindings[op->name] = op->value;
         op->value.accept(this);
         op->body.accept(this);
     }
@@ -50,6 +96,10 @@ class SymbolicComplexity : public IRVisitor {
 
         op->b.accept(this);
     }
+    void visit(const Variable *op) override {
+        debug(2) << "Visiting var: " << op->name << "\n";
+        numVars++;
+    }
 
 public:
     SymbolicComplexity() = default;
@@ -57,26 +107,32 @@ public:
     int rawFor;
     Expr rawAdds = 0;
     int depth = 0;
+    int numVars = 0;
     Expr totalRange;
+    std::map<std::string, Expr> bindings;
 };
 
 }  // namespace
 
 
 Pipeline compute_complexity(const Stmt &s) { 
-
+    debug(1) << "Beginning compute complexity\n";
+    debug(1) << "Stmt: " << s << "\n\n\n\n";
     SymbolicComplexity complexity;
     s.accept(&complexity);
 
     Expr e = complexity.totalRange;
     debug(1) << "Total range: " << e << "\n";
     debug(1) << "Total adds:" << complexity.rawAdds << "\n";
-
+    debug(1) << "got to beginning of compute complexity\n" << "\n";
     Func eq_adds;
-    eq_adds() = complexity.rawAdds;
+    Var x;
+    eq_adds(x) = complexity.rawAdds;
+    debug(1) << "added raw adds to eq_adds\n" << "\n";
     std::vector<Func> outputs;
     outputs.push_back(eq_adds);
     Pipeline p = Pipeline(outputs);
+    debug(1) << "got to end of compute complexity\n" << "\n";
     return p;
 }
 
