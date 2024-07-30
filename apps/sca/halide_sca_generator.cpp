ccbd2b178fd4fcc52160d657647f1229a6cdcedd
Vishal Canumalla <vishalc@cs.washington.edu>
2024-07-30 13:48:43 -0700

    store and load

--- apps/sca/halide_sca_generator.cpp
+++ apps/sca/halide_sca_generator.cpp
@@ -29,25 +29,64 @@ public:
     // GeneratorParam<int> tile_x{"tile_x", 32};  // X tile.
     // GeneratorParam<int> tile_y{"tile_y", 8};   // Y tile.
 
-    Input<Buffer<uint16_t, 2>> input{"input"};
-    Output<Buffer<uint16_t, 2>> blur_y{"blur_y"};
+    Input<Buffer<float, 2>> input{"input"};
+    Output<Buffer<float, 2>> blur_y{"blur_y"};
     void generate() {
         Var x("x_dim"), y("y_dim"), yi("yi_inner");
-        Func blur_x("blur_x");
-        // gradient(x, y) = x + y;
-        // gradient.split(x, x_outer, x_inner, 2);
-        // output(x, y) = cast(output.type(), gradient(x, y));
-        blur_x(x, y) = (input(x, y) + input(x + 1, y) + input(x + 2, y)) / 3;
-        blur_y(x, y) = (blur_x(x, y) + blur_x(x, y + 1) + blur_x(x, y + 2)) / 3;
-        blur_y
-            .split(y, y, yi, 32)
-            .parallel(y)
-            .vectorize(x, 16);
-        blur_x
-            .store_at(blur_y, y)
-            .compute_at(blur_y, x)
-            .vectorize(x, 16);
+
+        Func producer("producer"), consumer("consumer");
+        producer(x, y) = sin(x * y);
+
+        blur_y(x, y) = (producer(x, y) +
+                    producer(x, y + 1) +
+                    producer(x + 1, y) +
+                    producer(x + 1, y + 1)) / 4;
+
+        producer.compute_root();
+        consumer.trace_stores();
+        producer.trace_stores();
+        // blur_y(x, y) = consumer(x, y);
     }
 };
 }  // namespace
 HALIDE_REGISTER_GENERATOR(HalideSCA, halide_sca) 
+
+
+
+// Func producer("producer"), consumer("consumer");
+// producer(x, y) = sin(x * y);
+
+// consumer(x, y) = (producer(x, y) +
+//             producer(x, y + 1) +
+//             producer(x + 1, y) +
+//             producer(x + 1, y + 1)) / 4;
+
+
+
+
+
+// public:
+//     // GeneratorParam<int> tile_x{"tile_x", 32};  // X tile.
+//     // GeneratorParam<int> tile_y{"tile_y", 8};   // Y tile.
+
+//     Input<Buffer<uint16_t, 2>> input{"input"};
+//     Output<Buffer<uint16_t, 2>> output{"output"};
+//     void generate() {
+//         Var x("x_dim"), y("y_dim"), yi("yi_inner");
+//         Func blur_x("blur_x");
+//         Func clamped("clamped");
+//         Func blur_y("blur_y");
+//         clamped = Halide::BoundaryConditions::repeat_edge(input);
+
+
+//         blur_x(x, y) = (clamped(x, y) + clamped(x + 1, y) + clamped(x + 2, y)) / 3;
+//         blur_y(x, y) = (blur_x(x, y) + blur_x(x, y + 1) + blur_x(x, y + 2)) / 3;
+//         // blur_y
+//         //     .split(y, y, yi, 32)
+//         //     .parallel(y)
+//         //     .vectorize(x, 16);
+//         // blur_y.compute_root();
+//         output(x, y) = blur_y(x, y);
+//     }
+// };
+// }  // namespace
--- src/SymbolicComplexity.cpp
+++ src/SymbolicComplexity.cpp
@@ -13,6 +13,55 @@ namespace Internal {
 
 namespace {
 
+
+Expr inline_expr(std::map<std::string, Expr> bindings, Expr e) {
+    if (e.as<Variable>()) {
+        std::string name = e.as<Variable>()->name;
+        if (bindings.find(name) != bindings.end()) {
+            // return Expr(2 + 2);
+            return inline_expr(bindings, bindings[name]);
+        }
+        else {
+            return e;
+        }
+    }
+    else if (e.as<Sub>()) {
+        Expr left = e.as<Sub>()->a;
+        Expr right = e.as<Sub>()->b;
+        return inline_expr(bindings, left) - inline_expr(bindings, right);
+    }
+    else if (e.as<Add>()) {
+        Expr left = e.as<Add>()->a;
+        Expr right = e.as<Add>()->b;
+        return inline_expr(bindings, left) + inline_expr(bindings, right);
+    }
+    else if (e.as<Mul>()) {
+        Expr left = e.as<Mul>()->a;
+        Expr right = e.as<Mul>()->b;
+        return inline_expr(bindings, left) * inline_expr(bindings, right);
+    }
+    else if (e.as<Div>()) {
+        Expr left = e.as<Div>()->a;
+        Expr right = e.as<Div>()->b;
+        return inline_expr(bindings, left) / inline_expr(bindings, right);
+    }
+    else if (e.as<Mod>()) {
+        Expr left = e.as<Mod>()->a;
+        Expr right = e.as<Mod>()->b;
+        return inline_expr(bindings, left) % inline_expr(bindings, right);
+    }
+    else if (e.as<Min>()) {
+        Expr left = e.as<Min>()->a;
+        Expr right = e.as<Min>()->b;
+        return min(inline_expr(bindings, left), inline_expr(bindings, right));
+    }
+    else if (e.as<Max>()) {
+        Expr left = e.as<Max>()->a;
+        Expr right = e.as<Max>()->b;
+        return max(inline_expr(bindings, left), inline_expr(bindings, right));
+    }
+    return e;
+}
 class VariableBindings: public IRVisitor {
     using IRVisitor::visit;
     
@@ -30,36 +79,6 @@ class VariableBindings: public IRVisitor {
 
 class NumAdds: public IRVisitor {
     using IRVisitor::visit;
-    Expr inline_expr(Expr e) {
-        if (e.as<Variable>()) {
-            std::string name = e.as<Variable>()->name;
-            if (bindings.find(name) != bindings.end()) {
-                return bindings[name];
-            }
-        }
-        if (e.as<Sub>()) {
-            Expr left = e.as<Sub>()->a;
-            Expr right = e.as<Sub>()->b;
-            return inline_expr(left) - inline_expr(right);
-        }
-        if (e.as<Add>()) {
-            Expr left = e.as<Add>()->a;
-            Expr right = e.as<Add>()->b;
-            return inline_expr(left) + inline_expr(right);
-        }
-        if (e.as<Mul>()) {
-            Expr left = e.as<Mul>()->a;
-            Expr right = e.as<Mul>()->b;
-            return inline_expr(left) * inline_expr(right);
-        }
-        if (e.as<Div>()) {
-            Expr left = e.as<Div>()->a;
-            Expr right = e.as<Div>()->b;
-            return inline_expr(left) / inline_expr(right);
-        }
-
-        return e;
-    }
     void visit(const For *op) {
         factor = factor * (op->extent - op->min);
         op->body.accept(this);
@@ -67,34 +86,98 @@ class NumAdds: public IRVisitor {
     }
     void visit(const Add *op) {
         op->a.accept(this);
-        numAdds = numAdds + inline_expr(factor);
+        numAdds = numAdds + inline_expr(bindings, factor);
         op->b.accept(this);
     }
     public:
 
-        NumAdds() = default;
         NumAdds(std::map<std::string, Expr> b) : bindings(b) {}
         std::map<std::string, Expr> bindings;
         Expr factor = 1;
         Expr numAdds = 0;
 };
 
+class NumStores : public IRVisitor {
+    using IRVisitor::visit;
+    void visit(const Store *op) {
+        op->value.accept(this);
+        op->index.accept(this);
+        numStores = numStores + inline_expr(bindings, factor);
+    }
+    void visit(const For *op) {
+        factor = factor * (op->extent - op->min);
+        op->body.accept(this);
+        factor = factor / (op->extent - op->min);
+    }
 
+    public:
+
+        NumStores(std::map<std::string, Expr> b) : bindings(b) {}
+        std::map<std::string, Expr> bindings;
+        Expr factor = 1;
+        Expr numStores = 0;
+};
+
+class NumLoads : public IRVisitor {
+    using IRVisitor::visit;
+    void visit (const Load *op) {
+        op->index.accept(this);
+        numLoads = numLoads + factor;
+    }
+    void visit(const For *op) {
+        factor = factor * (op->extent - op->min);
+        op->body.accept(this);
+        factor = factor / (op->extent - op->min);
+    }
+    public:
+
+        NumLoads(std::map<std::string, Expr> b) : bindings(b) {}
+        std::map<std::string, Expr> bindings;
+        Expr factor = 1;
+        Expr numLoads = 0;
+    
+};
 }  // namespace
 
+void print_bindings(std::map<std::string, Expr> bindings) {
+    for (auto it = bindings.begin(); it != bindings.end(); it++) {
+        std::string name = it->first;
+        Expr value = it->second;
+        debug(1) << name << " = " << value << "\n";
+    }
+}
 
 Pipeline compute_complexity(const Stmt &s) { 
+    debug(1) << "\n\n\n\ncompute_complexity:\n" << s << "\n\n\n\n";
     std::vector<Func> outputs;
+
     // bind variables to symbolic values
     VariableBindings vb;
     s.accept(&vb);
+    auto bindings = vb.bindings;
+    print_bindings(vb.bindings);
 
     // run number of adds visitor
-    NumAdds na(vb.bindings);
+    NumAdds na(bindings);
     s.accept(&na);
     Func numAdds("numAdds");
     numAdds() = na.numAdds;
     outputs.push_back(numAdds);
+
+    NumStores ns(bindings);
+    s.accept(&ns);
+    Func numStores("numStores");
+    numStores() = ns.numStores;
+    outputs.push_back(numStores);
+    debug(1) << "numStores: " << ns.numStores << "\n";
+    
+
+    NumLoads nl(bindings);
+    s.accept(&nl);
+    Func numLoads("numLoads");
+    numLoads() = nl.numLoads;
+    outputs.push_back(numLoads);
+    debug(1) << "numLoads: " << nl.numLoads << "\n";
 
     Pipeline p = Pipeline(outputs);
     return p;
