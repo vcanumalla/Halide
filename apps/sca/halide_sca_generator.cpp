a5155cfa4474a0fe9ef88b8311af34c90f517ab3
Vishal Canumalla <vishalc@cs.washington.edu>
2024-08-07 14:57:16 -0700

    Update halide_sca_generator.cpp

--- apps/sca/halide_sca_generator.cpp
+++ apps/sca/halide_sca_generator.cpp
@@ -1,55 +1,55 @@
-#include "Halide.h"
-namespace {
-class HalideSCA : public Halide::Generator<HalideSCA> {
-public:
-    Input<Buffer<int32_t, 2>> input{"input"};
-    Output<Buffer<int32_t, 2>> output{"output"};
-    void generate() {
-        Var x, y;
-        output(x, y) = input(x, y) + 5;
-    }
-};
-
-
-
-
-
-
-}  // namespace
-HALIDE_REGISTER_GENERATOR(HalideSCA, halide_sca) 
-
-
-
-
-
 // #include "Halide.h"
 // namespace {
 // class HalideSCA : public Halide::Generator<HalideSCA> {
 // public:
-//     // GeneratorParam<int> tile_x{"tile_x", 32};  // X tile.
-//     // GeneratorParam<int> tile_y{"tile_y", 8};   // Y tile.
-
-//     Input<Buffer<float, 2>> input{"input"};
-//     Output<Buffer<float, 2>> blur_y{"blur_y"};
+//     Input<Buffer<int32_t, 2>> input{"input"};
+//     Output<Buffer<int32_t, 2>> output{"output"};
 //     void generate() {
-//         Var x("x_dim"), y("y_dim"), yi("yi_inner");
-
-//         Func producer("producer"), consumer("consumer");
-//         producer(x, y) = sin(x * y);
-
-//         blur_y(x, y) = (producer(x, y) +
-//                     producer(x, y + 1) +
-//                     producer(x + 1, y) +
-//                     producer(x + 1, y + 1)) / 4;
-
-//         // producer.compute_root();
-//         consumer.trace_stores();
-//         producer.trace_stores();
-//         // blur_y(x, y) = consumer(x, y);
+//         Var x, y;
+//         output(x, y) = input(x, y) + 5;
 //     }
 // };
+
+
+
+
+
+
 // }  // namespace
 // HALIDE_REGISTER_GENERATOR(HalideSCA, halide_sca) 
+
+
+
+
+
+#include "Halide.h"
+namespace {
+class HalideSCA : public Halide::Generator<HalideSCA> {
+public:
+    // GeneratorParam<int> tile_x{"tile_x", 32};  // X tile.
+    // GeneratorParam<int> tile_y{"tile_y", 8};   // Y tile.
+
+    Input<Buffer<float, 2>> input{"input"};
+    Output<Buffer<float, 2>> blur_y{"blur_y"};
+    void generate() {
+        Var x("x_dim"), y("y_dim"), yi("yi_inner");
+
+        Func producer("producer"), consumer("consumer");
+        producer(x, y) = sin(x * y);
+
+        blur_y(x, y) = (producer(x, y) +
+                    producer(x, y + 1) +
+                    producer(x + 1, y) +
+                    producer(x + 1, y + 1)) / 4;
+
+        producer.compute_root();
+        // consumer.trace_stores();
+        // producer.trace_stores();
+        // blur_y(x, y) = consumer(x, y);
+    }
+};
+}  // namespace
+HALIDE_REGISTER_GENERATOR(HalideSCA, halide_sca) 
 
 
 
